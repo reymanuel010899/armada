8ac74571e28d95b3f81de10229fc39e3ea983274
reymanuel010899 <reymanuelferrera78@gmail.com>
2023-04-22 19:30:14 -0400

    first commit

--- armada/settings.py
+++ armada/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n(^uu34=h6@c04459s43)u=5frz5@eg0gop0fi!x9#g4wybw6z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://armada-production.up.railway.app']
